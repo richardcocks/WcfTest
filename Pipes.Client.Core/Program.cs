ea4c2388150c94e8f3a005b2101303ff557e3b79
rcocks <richard.cocks@gmail.com>
2025-05-17 09:16:34 +0100

    System.IO.Pipes

---
+++ Pipes.Client.Core/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.IO.Pipes;
+using RandomSource;
+
+namespace Pipes.Client.Core;
+
+public static class Program
+{
+	static async Task Main()
+    {
+       
+        var pipeClient = new NamedPipeClientStream(".", "net.pipe://randomtest", PipeDirection.InOut, PipeOptions.Asynchronous);
+	
+        using var cts = new CancellationTokenSource(10_000);
+	
+        await pipeClient.ConnectAsync(30, cts.Token);
+	
+        var buffer = new byte[4];
+        var arrayPool = ArrayPool<byte>.Create();
+        var bag = new HashSet<long>();
+		
+        var sw = Stopwatch.StartNew();
+        while(!cts.IsCancellationRequested){
+            await pipeClient.ReadExactlyAsync(buffer, cts.Token);
+            var length = BitConverter.ToInt32(buffer);
+            var streamBuffer = ArrayPool<byte>.Shared.Rent(length);
+		
+            // We can't use ReadExactlyAsync because streamBuffer might be the wrong length
+            _ = await pipeClient.ReadAsync(streamBuffer, cts.Token);
+            var sequenceVals = MessagePack.MessagePackSerializer.Deserialize<IEnumerable<SequenceValue>>(streamBuffer,MessagePack.Resolvers.ContractlessStandardResolver.Options);	
+		
+            foreach (var element in sequenceVals)
+            {
+	            //Console.WriteLine($"Adding {element.Sequence} - {element.Value}");
+	            if (bag.Add(element.Value)) continue;
+	            sw.Stop();
+                Console.WriteLine($"Found duplicate {element.Value} at {element.Sequence} in {sw.ElapsedMilliseconds}ms");
+                await cts.CancelAsync();
+                return;
+            }
+		}
+	}
+}
---
+++ Pipes.Server.Core/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Pipes;
+using RandomSource;
+
+namespace Pipes.Server.Core;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await using var pipeServer = new NamedPipeServerStream("net.pipe://randomtest", PipeDirection.InOut, -1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+        
+        await pipeServer.WaitForConnectionAsync();
+
+        var random = new RandomProvider(new Random(0), 32);
+        using var cts = new CancellationTokenSource(30_000);
+
+        
+        while (!cts.IsCancellationRequested && pipeServer.IsConnected)
+        {
+            var buffer = random.Take(1_000);
+
+            var byteBuffer = MessagePack.MessagePackSerializer.Serialize(buffer, MessagePack.Resolvers.ContractlessStandardResolver.Options);
+
+            var lengthBytes = BitConverter.GetBytes(byteBuffer.Length);
+            
+            await pipeServer.WriteAsync(lengthBytes, cts.Token);
+            await pipeServer.WriteAsync(byteBuffer, cts.Token);
+        }
+    }
+}
